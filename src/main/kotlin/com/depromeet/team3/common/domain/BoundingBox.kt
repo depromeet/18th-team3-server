a30ffea1fdacc587ae6c5f3e2c47c5600561729c
조재중 <126754298+m-a-king@users.noreply.github.com>
2026-04-08 21:02:30 +0900

    docs: BoundingBox 주석의 필드명을 camelCase 로 통일
    - pixelX = xmin / ... → pixelX = xMin / ...
    - 앞선 커밋에서 정리한 camelCase 네이밍과 일치하도록 주석 업데이트

--- src/main/kotlin/com/depromeet/team3/common/domain/BoundingBox.kt
+++ src/main/kotlin/com/depromeet/team3/common/domain/BoundingBox.kt
@@ -2,10 +2,10 @@ package com.depromeet.team3.common.domain
 
 /**
  * 원본 이미지의 실제 픽셀로 변환하려면:
- *   pixelX = xmin / 1000.0 * imageWidth
- *   pixelY = ymin / 1000.0 * imageHeight
- *   pixelWidth = (xmax - xmin) / 1000.0 * imageWidth
- *   pixelHeight = (ymax - ymin) / 1000.0 * imageHeight
+ *   pixelX = xMin / 1000.0 * imageWidth
+ *   pixelY = yMin / 1000.0 * imageHeight
+ *   pixelWidth = (xMax - xMin) / 1000.0 * imageWidth
+ *   pixelHeight = (yMax - yMin) / 1000.0 * imageHeight
  */
 data class BoundingBox(
     val yMin: Int,
