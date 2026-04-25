884110f89bc189f8c515dea9e61a7e56ba2016fb
1o18z <qkrdmswl1018@naver.com>
2026-04-25 21:54:37 +0900

    feat: 공통 BaseEntity 추상 클래스 추가
    - createdAt, updatedAt, deletedAt 감사 컬럼 정의
    - Hibernate 프록시를 고려한 equals/hashCode 구현

---
+++ src/main/kotlin/com/depromeet/team3/common/domain/BaseEntity.kt
@@ -0,0 +1,41 @@
+package com.depromeet.team3.common.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.Hibernate
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity<ID : Any> {
+
+    abstract fun getId(): ID
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime? = null
+        protected set
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime? = null
+        protected set
+
+    @Column(name = "deleted_at")
+    var deletedAt: LocalDateTime? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is BaseEntity<*>) return false
+        if (Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        return getId() == other.getId()
+    }
+
+    override fun hashCode(): Int = Hibernate
+        .getClass(this)
+        .hashCode() * 31 + getId().hashCode()
+}
